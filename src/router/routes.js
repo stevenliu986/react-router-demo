00f22fcced96c5abda588040f19aa80fa2f5bb2b
StevenLiu <stevenliu986@gmail.com>
2023-07-21 18:45:21 +1000

    modified:   src/router/routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,5 @@
-/* 
-  配置路由表: 形式为一个数组。该数组中的每一项就是每一个需要配置的路由规则。
-    redirect: 此配置是重定向
-    from: 来源的地址
-    to: 重定向的地址
-    exact：是否精准匹配
-    path: 匹配的路径
-    component：匹配的组件
-    name: 路由名称（命名路由）
-    meta: {} 路由元信息 - 包含当前路由的一些信息，当路由匹配后，可以用这些信息做一些事情
-    children：[] 子路由
-*/
-
+import { lazy } from "react";
 import A from "../views/A";
-import B from "../views/B";
-import C from "../views/C";
 import aRoutes from "./aRoutes";
 
 // 一级路由表
@@ -34,13 +20,15 @@ const routes = [
   {
     path: "/b",
     name: "b",
-    component: B,
+    // 在这里使用lazy方法加载组件
+    component: lazy(() => import("../views/B")),
     meta: {},
   },
   {
     path: "/c",
     name: "c",
-    component: C,
+    // 在这里使用lazy方法加载组件
+    component: lazy(() => import("../views/C")),
     meta: {},
   },
   {
